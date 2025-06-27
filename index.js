c2bf44989f746ade25a954b8c5c85afd0261156a
Carlos Binker <carlosbinker@gmail.com>
2025-06-27 18:15:25 -0300

    Generamos en el index.js las dos nuevas solicitadas, una que devuelve un HTML en / y otra que devuelve un json de productos en la ruta /products

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ import express from "express";
 // Creo la instancia
 const app = express();
 
-//Defino rutas con método GET
+// Agregado de una ruta de bienvenida que devuelve un HTML
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
+app.get("/", (req, res) => {
+  res.send("<h1>Bienvenido a la API REST</h1>");
 });
+
+// Agregado de una ruta que devuelve un JSON
+
+const products = [
+  { id: 1, nombre: "queso fresco", precio: 800, vencimiento: "30/11/25" },
+  { id: 2, nombre: "queso roquefort", precio: 1200, vencimiento: "3/10/25" },
+  { id: 3, nombre: "queso port salut", precio: 650, vencimiento: "16/8/25" },
+  { id: 4, nombre: "queso cuartirolo", precio: 860, vencimiento: "13/9/26" },
+];
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+
 
 //Escucho en el puerto 3000
 

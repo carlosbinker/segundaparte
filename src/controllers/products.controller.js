535594c20555f21ae015672f8cc5e9de04cee06b
Carlos Binker <carlosbinker@gmail.com>
2025-07-02 17:39:27 -0300

    mando los datos de los productos a products.service.js

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,21 +1,22 @@
 // Listado de productos (array con objetos JSON) -- Lo mudamos ahora desde el módulo products.router.js
 
-const products = [
-  {
-    id: 1,
-    nombre: "queso fresco rallado",
-    precio: 800,
-    vencimiento: "30/11/25",
-  },
-  { id: 2, nombre: "queso roquefort", precio: 1200, vencimiento: "3/10/25" },
-  { id: 3, nombre: "queso port salut", precio: 650, vencimiento: "16/8/25" },
-  { id: 4, nombre: "queso cuartirolo", precio: 860, vencimiento: "13/9/26" },
-];
+// const products = [
+//   {
+//     id: 1,
+//     nombre: "queso fresco rallado",
+//     precio: 800,
+//     vencimiento: "30/11/25",
+//   },
+//   { id: 2, nombre: "queso roquefort", precio: 1200, vencimiento: "3/10/25" },
+//   { id: 3, nombre: "queso port salut", precio: 650, vencimiento: "16/8/25" },
+//   { id: 4, nombre: "queso cuartirolo", precio: 860, vencimiento: "13/9/26" },
+// ];
+
+import * as service from '../services/products.service.js'
 
 // Exporto el controlador que me va a traer toods los productos desde el array ficticio que me simula por ahora el modelo de la BD
 export const getAllProducts = (req, res) => {
-    res.send(products);
-    console.log(products)
+    res.send(service.getAllProducts());
 };
 
 export const searchProducts = (req, res) => {
---
+++ src/services/products.service.js
@@ -0,0 +1,15 @@
+// Listado de productos (array con objetos JSON) -- Lo mudamos ahora desde el módulo products.controllers.js. Simularemos por ahora que hay un servicio que le brindará los datos a los controladores.
+
+const products = [
+  {
+    id: 1,
+    nombre: "queso fresco rallado",
+    precio: 800,
+    vencimiento: "30/11/25",
+  },
+  { id: 2, nombre: "queso roquefort", precio: 1200, vencimiento: "3/10/25" },
+  { id: 3, nombre: "queso port salut", precio: 650, vencimiento: "16/8/25" },
+  { id: 4, nombre: "queso cuartirolo", precio: 860, vencimiento: "13/9/26" },
+];
+
+export const getAllProducts = () => { return products };

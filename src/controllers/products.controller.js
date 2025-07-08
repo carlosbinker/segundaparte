743f4484252a89ce36ba2c41a61ca28e85c985fd
Carlos Binker <carlosbinker@gmail.com>
2025-07-08 10:11:48 -0300

    Ejercicio 13 completado, ahora todos los datos se manejan desde el modelo por parte de los controladores. En models se define directamente un archivo json denominado products.json, y todo se lee desde ahí simulando la BD

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,53 +1,39 @@
-import * as service from '../services/products.service.js'
+import * as model from '../models/products.model.js'
 
-// Exporto el controlador que me va a traer toods los productos desde el array ficticio que me simula por ahora el modelo de la BD
-// Igual situación intentamos realizar para el resto de los métodos
+// Método GET - obtengo todos los productos de un JSON
 export const getAllProducts = (req, res) => {
-  res.send(service.getAllProducts());
-  console.log(service.getAllProducts())
+  res.send(model.getAllProducts());
 };
 
-// Método GET, recibiendo query string en la URL
+// Método GET - recibiendo query string en la URL en req.query
 export const searchProducts = (req, res) => {
   const { nombre } = req.query;
-
-  // La búsqueda por nombre se resuleve en el servicio. El controlador sólo devuelve como respuesta los productos filtrados.
-
-  const filteredProducts = service.searchProducts(nombre);
+  const filteredProducts = model.searchProducts(nombre);
   res.json(filteredProducts);
-  console.log("Los productos filtrados son:\n", filteredProducts);
 };
 
-// Método GET, recibiendo un id como params en la URL
+// Método GET - recibiendo un id como params en la URL en req.params
 export const getProductById = (req, res) => {
   const { id } = req.params;
-
-  const product = service.getProductById(id);
-
+  const product = model.getProductById(id);
   if (!product) {
-    // si product me da un undefined
     res.status(404).json({ error: "No existe el producto" });
   }
   res.json(product);
-  console.log("El producto encontrado es:\n", product);
 };
 
-// Método POST - añadir nuevo producto, el nuevo objeto se recibe mediante body
+// Método POST - añadir nuevo producto, el nuevo objeto se recibe mediante req.body
 export const addNewProduct = (req, res) => {
   const { nombre, precio, vencimiento } = req.body;
-  const newProduct = service.addNewProduct({ nombre, precio, vencimiento});
-
+  const newProduct = model.addNewProduct({ nombre, precio, vencimiento});
   res.status(201).json(newProduct);
-  console.log("El producto añadido es:\n", newProduct);
 };
 
-// Método PUT - Update Product
+// Método PUT - Update Product, la actualización del producto se recibe por req.body al igual que POST
 export const updateProductById = (req, res) => {
   const productId = parseInt(req.params.id, 10);
   const { nombre, precio, vencimiento } = req.body;
-
-  const updateProduct = service.updateProductById(productId, {nombre, precio, vencimiento});
-
+  const updateProduct = model.updateProductById(productId, {nombre, precio, vencimiento});
   if (updateProduct === -1) {
     return res.status(404).json({ error: "Producto no encontrado" });
   }
@@ -55,16 +41,12 @@ export const updateProductById = (req, res) => {
   console.log("El producto actualizado es:\n", updateProduct);
 };
 
-// Delete Product
+// Método DELETE - Delete Product
 export const deleteProductById = (req, res) => {
   const productId = parseInt(req.params.id, 10);
-
-  const deletedProduct = service.deletedProduct(productId);
-  // const productIndex = service.getAllProducts().findIndex((objeto) => objeto.id == productId);
-
+  const deletedProduct = model.deletedProduct(productId);
   if (deletedProduct == -1) {
     return res.status(404).json({ error: "Producto no encontrado para su borrado" });
   }
-  // service.getAllProducts().splice(productIndex, 1); // Indica que me quita 1 elemento del array
   res.status(204).send(); // La convención 204 indica que se borra un recurso y el send() indica que no tiene contenido
 };
---
+++ src/models/products.model.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+
+const __dirname = import.meta.dirname;
+
+const jsonPath = path.join(__dirname, "./products.json");
+const json = fs.readFileSync(jsonPath, "utf-8");
+const products = JSON.parse(json); // Transformo el JSON en un objeto javascript
+
+// Métodos GET
+export const getAllProducts = () => {return products};
+    
+export const searchProducts = (nombre) => {return products.filter((objeto) => objeto.nombre.toLowerCase().includes(nombre.toLowerCase()))};
+
+export const getProductById = (id) => {return products.find((objeto) => id == objeto.id)};
+
+// Método POST - Agrrego producto
+export const addNewProduct = (data) => {
+  const newProduct = {
+    id: products.length + 1,
+    ...data,
+  };
+  products.push(newProduct);
+  return newProduct;
+};
+
+// Método PUT - Actualizo el producto en el array de productos, según el id recibido
+export const updateProductById = (productId, data) => {
+  const productIndex = products.findIndex((objeto) => objeto.id === productId);
+  products[productIndex] = {
+    id: productIndex+1,
+    ...data
+  };
+  return products[productIndex];
+};
+
+// Método DELETE - Borro el producto dado por el Id
+export const deletedProduct = (productId) => {
+  const productIndex = products.findIndex((objeto) => objeto.id === productId);
+  return products.splice(productIndex, 1); 
+};
